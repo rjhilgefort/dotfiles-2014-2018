2dae444f858d5e720e261e6af18f9cd156f07c1d
Rob Hilgefort <rjhilgefort@gmail.com>
2014-08-08 11:18:55 -0400

    cleanup zipscene-startuup script

--- bin/zipscene-startup.sh
+++ bin/zipscene-startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Start mongo in background
 mongod &
-node-inspector &
+
+# ssh tunnel to qa.zipscene.com
 $(ssh -o "ServerAliveInterval 10" -o "TCPKeepAlive yes" -o "BatchMode yes" -L 27018:127.0.0.1:27017 rhilgefort@qa.zipscene.com &> /dev/null) &
+
+# node-inspector &
