845e57e1d3139da722fba1c4fb26ab532f06912b
Rob Hilgefort <rjhilgefort@gmail.com>
2014-12-06 17:15:10 -0500

    Overhauled slate file

--- .slate.js
+++ .slate.js
@@ -1,14 +1,21 @@
-// source relsate.js from submodule
+/**
+ * Keys:
+ * - ctrl
+ * - alt
+ * - cmd
+ */
+
+// Source relsate.js from submodule
 S.src('.src/reslate/reslate.js');
 
-// enable to see debug messages in Console.app
+// Enable to see debug messages in Console.app
 $.debug = true;
 
-// slate.alias('hyper', 'ctrl;alt;cmd');
-slate.alias('hyper', 'cmd;shift');
-slate.alias('augment', 'ctrl');
+$.log('LOADING CONFIG');
 
-// begin config
+slate.alias('hyper', 'cmd;alt;ctrl');
+
+// Begin config
 slate.configAll({
 	defaultToCurrentScreen: true,
 	nudgePercentOf: 'screenSize',
@@ -26,87 +33,99 @@ slate.configAll({
 	]
 });
 
-// bindings
+
+// Layouts
+// TODO:
+
+// Bindings
 slate.bindAll({
 
 	hyper: {
 
-		// Augment hyper shortcuts for more options
-		augment: {
+		// Direct focus to App
+		// 's' because it's easy to hit with hyper.
+		// All the below keys are right handed (and should stay that way)
+		shift: {
+			// Primary, most common
+			j: $.focus('iTerm2'),
+			k: $.focus('Google Chrome'),
+			l: [$.focus('Messages'),
+				$.focus('Hipchat')],
+			';': $.focus('Dash'),
+			// h: // TODO: Focus previous
 
-			// Edges
-			h: [$('barResize', 'left',   3),
-				$('center',    'left',   3, 3)],
-			j: [$('barResize', 'bottom', 2),
-				$('center',    'bottom', 3, 3)],
-			k: [$('barResize', 'top',    2),
-				$('center',    'top',    3, 3)],
-			l: [$('barResize', 'right',  3),
-				$('center',    'right',  3, 3)],
-
-			// Corners
-			y: [$('corner', 'top-left',     3, 2),
-				$('corner', 'top-left',     3, 3)],
-			i: [$('corner', 'top-right',    3, 2),
-				$('corner', 'top-right',    3, 3)],
-			b: [$('corner', 'bottom-left',  3, 2),
-				$('corner', 'bottom-left',  3, 3)],
-			m: [$('corner', 'bottom-right', 3, 2),
-				$('corner', 'bottom-right', 3, 3)],
-
-			// Centers
-			u: [$('center', 'top'),
-				$('center', 'top', 3, 3)],
-			n: [$('center', 'bottom'),
-				$('center', 'bottom', 3, 3)],
-			'return': $('center', 'center', 1.25, 1.25)
-
+			// Secondary, second most common
+			u: $.focus('Spotify'),
+			i: [$.focus('Airmail'),
+				$.focus('Sunrise')],
+			o: $.focus('Finder'),
+			y: [$.focus('Reminders'),
+				$.focus('Notes')],
 		},
 
 
 		// Bars
-		h: [$('barResize', 'left',  2),
-			$('barResize', 'left',  1.5)],
-		j: $('barResize', 'bottom', 2),
-		k: $('barResize', 'top',    2),
+		h: [$('barResize', 'left', 2),
+			$('barResize', 'left', 1.5),
+			$('barResize', 'left', 3)/*,
+			$('center',    'left', 3, 3)*/],
+
+		j: [$('barResize', 'bottom', 2),
+			$('barResize', 'bottom', 2)/*,
+			$('center',    'bottom', 3, 3)*/],
+
+		k: [$('barResize', 'top', 2),
+			$('barResize', 'top', 2)/*,
+			$('center',    'top', 3, 3)*/],
+
 		l: [$('barResize', 'right', 2),
-			$('barResize', 'right', 1.5)],
-			'return': $('barResize', 'top', 1),
+			$('barResize', 'right', 1.5),
+			$('barResize', 'right', 3)/*,
+			$('center',    'right', 3, 3)*/],
 
 		// Corners
 		y: [$('corner', 'top-left'),
-			$('corner', 'top-left', 1.5)],
+			$('corner', 'top-left', 1.5),
+			$('corner', 'top-left', 3, 2),
+			$('corner', 'top-left', 3, 3)],
+
 		i: [$('corner', 'top-right'),
-			$('corner', 'top-right', 1.5)],
+			$('corner', 'top-right', 1.5),
+			$('corner', 'top-right', 3, 2),
+			$('corner', 'top-right', 3, 3)],
+
 		b: [$('corner', 'bottom-left'),
-			$('corner', 'bottom-left', 1.5)],
+			$('corner', 'bottom-left', 1.5),
+			$('corner', 'bottom-left', 3, 2),
+			$('corner', 'bottom-left', 3, 3)],
+
 		m: [$('corner', 'bottom-right'),
-			$('corner', 'bottom-right', 1.5)],
+			$('corner', 'bottom-right', 1.5),
+			$('corner', 'bottom-right', 3, 2),
+			$('corner', 'bottom-right', 3, 3)],
+
 
 		// Centers
-		u: $('center', 'top'),
-		n: $('center', 'bottom'),
+		u: [$('center', 'top'),
+			$('center', 'top', 3, 3)],
+		n: [$('center', 'bottom'),
+			$('center', 'bottom', 3, 3)],
+
+		// Maximize
+		'return': [$('barResize', 'top', 1),
+					$('center', 'center', 1.25, 1.25)],
+
 
 		// Throw to monitor
 		'`': ['throw 0 resize', 'throw 1 resize'],
 		'1': $('toss', '0', 'resize'),
 		'2': $('toss', '1', 'resize'),
-		'3': $('toss', '2', 'resize'),
 
-		// Direct focus
-		w: $.focus('Google Chrome'),
-		c: $.focus('Messages'),
-		d: $.focus('Dash'),
-		t: $.focus('iTerm2'),
-		f: $.focus('Finder'),
-		a: $.focus('Airmail'),
-		s: $.focus('Spotify'),
-		r: $.focus('Reminders'),
 
 		// Utility functions
 		f1: 'relaunch',
 		z: 'undo',
-		tab: 'hint'
+		// tab: 'hint',
 
 	},
 
@@ -114,3 +133,5 @@ slate.bindAll({
 	'esc:cmd': 'hint'
 
 });
+
+$.log('DONE LOADING CONFIG');
