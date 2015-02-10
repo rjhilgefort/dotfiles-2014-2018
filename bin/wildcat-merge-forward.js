6c0a257c940a0ac4832e178a50d6d761abed1942
Rob Hilgefort <rjhilgefort@gmail.com>
2015-02-10 16:23:23 -0500

    Adding a helper utility to merge release branches forward in wildcat

---
+++ bin/wildcat-merge-forward.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+// Declare common vars
+var shell, _, branches, setupBranch, mergeFrom, silent, output;
+
+shell = require('shelljs');
+_ = require('lodash');
+
+
+// Make sure branch is up to date locally and set stage
+setupBranch = function(branch) {
+	shell.echo("\n\n\n" + "==> Processing: " + branch + "\n");
+	shell.exec('git co ' + branch);
+	shell.exec('git pull');
+};
+
+// Merge from branch into current
+mergeFrom = function(branch) {
+	output = shell.exec('git merge ' + branch);
+
+	// Handle conflicts
+	if (/CONFLICT/.test(output.output)) {
+		shell.exec("git mergetool");
+		shell.exec("git add -A");
+		shell.exec("git commit --no-edit");
+	}
+
+	shell.exec('git push');
+};
+
+// Shortcut
+silent = { silent: true };
+
+///////////////////////////////////////////////////////////////////////////////
+// Start Script
+///////////////////////////////////////////////////////////////////////////////
+
+shell.cd(shell.env.HOME + '/Projects/zipscene/wildcat');
+
+// Update remotes
+shell.exec('git fetch');
+
+// Get branches
+branches = shell.exec('git branches | ag "remotes.*release"', silent);
+if (branches.code !== 0) {
+	shell.echo("No branches found to merge to/with");
+	shell.exit(1);
+}
+
+// Parse branches
+branches = branches.output;
+branches = branches.split('\n');
+_.forEach(branches, function(branch, i) {
+	branch = branch.trim();
+	branch = branch.replace('remotes/origin/', '');
+	branches[i] = branch;
+});
+// Parse out empty strings and anything else that happens to be falsy
+branches = _.compact(branches);
+
+// Merge all the found branches
+_.forEach(branches, function(branch, i) {
+	setupBranch(branch);
+
+	// Skip merge process when processing first
+	if (i === 0) return true;
+
+	mergeFrom(branches[(i - 1)]);
+});
+
+// Finally, merge into master
+setupBranch('master');
+mergeFrom(_.last(branches));
+
