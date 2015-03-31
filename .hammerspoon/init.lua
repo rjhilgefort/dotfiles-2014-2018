39bd3cb7d73032801d03069be3a752355e21b002
Rob Hilgefort <rjhilgefort@gmail.com>
2015-03-30 21:40:33 -0400

    Adding initial hammerspoon config, reload config file automagically

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+-- Smart config reloading
+function reloadConfig(files)
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            hs.reload()
+            break
+        end
+    end
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Hammerspoon Config Loaded")
