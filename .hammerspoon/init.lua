a9c37479167193cd409a0d52e31000d68f5f112d
Rob Hilgefort <rjhilgefort@gmail.com>
2015-03-30 22:12:28 -0400

    Playing around with/in lua

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,19 +1,29 @@
+local hyper = { 'cmd', 'alt', 'ctrl' }
+local hammerSpoonConfigDir = os.getenv("HOME") .. "/.hammerspoon/"
 
-
-
-
-
-
+hs.hotkey.bind(hyper, 'c', function()
+    hs.alert.show('DOING THE THING YOU WANTED')
+    print()
+end)
 
 
 -- Smart config reloading
 function reloadConfig(files)
     for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
+        if file:sub(-4) == '.lua' then
             hs.reload()
             break
         end
     end
 end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Hammerspoon Config Loaded")
+
+hs.pathwatcher
+    .new(hammerSpoonConfigDir, reloadConfig)
+    :start()
+
+hs.notify.new({
+        title = "Hammerspoon Config Loaded",
+        informativeText = "Change Detected In: \n" .. hammerSpoonConfigDir
+    })
+    :send()
+    :release()
