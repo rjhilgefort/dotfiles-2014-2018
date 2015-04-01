becf7f125d85aed216f8dd29c17100226a317433
Rob Hilgefort <rjhilgefort@gmail.com>
2015-04-01 10:44:58 -0400

    Fixed hammerspoon requires

--- .hammerspoon/lib/apps.lua
+++ .hammerspoon/lib/apps.lua
@@ -1,19 +1,19 @@
 apps = {}
 
-apps.launchApps = {
+apps.toLaunch = {
     'iTerm', 'Google Chromee', 'Finder', 'Sonos', 'Messages', 'Spotify',
     'Simplify', '1Password mini', 'Clear', 'Sunrise', 'Airmail Beta',
     'Dash', 'Blink1Control'
 }
-apps.killApps = {
-    'iTunes',   'System Preferences', '1Password'
+apps.toKill = {
+    'iTunes',   'System Preferences', '1Password', 'Battle.net'
 }
 
 -- TODO:
 -- Capture all visible windows on all screens before doing anything else, then
 --      refocus them after launching all the things
 function apps.launchApps()
-    for i,app in pairs(apps.launchApps) do
+    for i,app in pairs(apps.toLaunch) do
         hs.application.launchOrFocus(app)
         app = hs.appfinder.appFromName(app)
         if (app ~= nil) then app:hide() end
@@ -23,6 +23,6 @@ end
 -- Kill any running apps in the kill list
 function apps.killApps()
     for i,app in pairs(hs.application.runningApplications()) do
-        if (hs.fnutils.contains(apps.killApps, app:title())) then app:kill() end
+        if (hs.fnutils.contains(apps.toKill, app:title())) then app:kill() end
     end
 end
