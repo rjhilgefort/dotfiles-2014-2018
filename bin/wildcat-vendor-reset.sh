864626b95958489fa1778effb4aacb06bb856a58
Rob Hilgefort <rjhilgefort@gmail.com>
2015-05-27 10:27:50 -0400

    wildcat vendor reset for any branch

--- bin/wildcat-vendor-reset.sh
+++ bin/wildcat-vendor-reset.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-rm -rf node_modules bower_components plugman
-npm install
+# clear any/all vendors
+rm -rf vendor # old branches ignore this, cli branches store stuff here
+rm -rf node_modules
+rm -rf bower_components # cli branches install bower stuff here
+rm -rf plugman
 
+git reset --hard
+
+npm install
