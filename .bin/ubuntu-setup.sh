0b8051e75d94fc86af85089855883ffa1989728d
Rob Hilgefort <rjhilgefort@gmail.com>
2015-09-10 18:36:16 -0400

    ubuntu ruby deps

--- .bin/ubuntu-setup.sh
+++ .bin/ubuntu-setup.sh
@@ -3,7 +3,13 @@
 # Make sure we're up to date
 sudo apt-get update
 
+# Ruby, RubyGems
+sudo apt-get install ruby-full build-essential
+# Gems
+gem install jekyll
+
 # pandoc
 sudo apt-get install pandoc
+
 # ranger
 sudo apt-get install ranger caca-utils highlight atool w3m poppler-utils mediainfo
