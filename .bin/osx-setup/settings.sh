a89b8b0cb47b2151127a4505f86c7b3cd0828fc8
Rob Hilgefort <rjhilgefort@gmail.com>
2016-10-31 23:49:14 -0400

    Add screenshot handling to mac setup script

--- .bin/osx-setup/settings.sh
+++ .bin/osx-setup/settings.sh
@@ -12,3 +12,7 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# Set screenshot folder to dropbox
+defaults write com.apple.screencapture location ~/$(whoami)/Dropbox/Screenshots
+killall SystemUIServer # TODO: Probably worth moving this to EOF
