15d694a17228bf0c55c9d17bebe30afdde7562c8
Rob Hilgefort <rjhilgefort@gmail.com>
2016-11-15 11:53:04 -0500

    Update casks, reorganized a bit

--- .bin/osx-setup/caskfile.sh
+++ .bin/osx-setup/caskfile.sh
@@ -6,60 +6,70 @@ brew tap caskroom/versions
 brew tap alehouse/homebrew-unofficial
 
 ###############################################################################
-# Core
+# Interface Apps
 ###############################################################################
 brew cask install google-chrome 2> /dev/null
+brew cask install firefox 2> /dev/null
 brew cask install dropbox 2> /dev/null
 brew cask install alfred 2> /dev/null
-
-brew cask install bartender 2> /dev/null
-brew cask install iterm2-borderless 2> /dev/null
-brew cask install slate 2> /dev/null
-brew cask install dash 2> /dev/null
-brew cask install evernote 2> /dev/null
 brew cask install spotify 2> /dev/null
-brew cask install firefox 2> /dev/null
-brew cask install vlc 2> /dev/null
-brew cask install 1password 2> /dev/null
 brew cask install sonos 2> /dev/null
-brew cask install karabiner-elements 2> /dev/null
-brew cask install hammerspoon 2> /dev/null
+brew cask install 1password 2> /dev/null
 brew cask install skype 2> /dev/null
-brew cask install disk-inventory-x 2> /dev/null
 brew cask install google-drive 2> /dev/null
-brew cask install adobe-reader 2> /dev/null
-brew cask install liteicon 2> /dev/null
+brew cask install hipchat 2> /dev/null
+brew cask install harvest 2> /dev/null
+brew cask install goofy 2> /dev/null
+
+###############################################################################
+# OS Usage Apps
+###############################################################################
+brew cask install bartender 2> /dev/null
+brew cask install slate 2> /dev/null
+brew cask install hammerspoon 2> /dev/null
+brew cask install evernote 2> /dev/null
+brew cask install karabiner-elements 2> /dev/null
 brew cask install istat-menus 2> /dev/null
 brew cask install duet 2> /dev/null
-brew cask install obs 2> /dev/null
-brew cask install soundflower 2> /dev/null
-brew cask install appcleaner 2> /dev/null
-brew cask install gitter 2> /dev/null
 
+###############################################################################
+# Coding
+###############################################################################
+brew cask install iterm2-borderless 2> /dev/null
+brew cask install dash 2> /dev/null
+brew cask install robomongo 2> /dev/null
+brew cask install gitter 2> /dev/null
 # brew cask install sublime-text3 2> /dev/null
 # brew cask install atom 2> /dev/null
-# brew cask install drobo-dashboard 2> /dev/null
-# brew cask install hipchat 2> /dev/null
 
 ###############################################################################
-# Nice To Have
+# Gaming
 ###############################################################################
-# brew cask install ksdiff 2> /dev/null
-# brew cask install flash 2> /dev/null
-brew cask install robomongo 2> /dev/null
 brew cask install steam 2> /dev/null
 # brew cask install origin 2> /dev/null
-# brew cask install virtualbox 2> /dev/null
-# brew cask install colloquy 2> /dev/null
-# brew cask install imagealpha 2> /dev/null
-# brew cask install torbrowser 2> /dev/null
-# brew cask install boxer 2> /dev/null
-# brew cask install pandoc 2> /dev/null
 brew cask install track-o-bot 2> /dev/null
+brew cask install obs 2> /dev/null
+brew cask install soundflower 2> /dev/null
 
 ###############################################################################
 # Utility
 ###############################################################################
+brew cask install appcleaner 2> /dev/null
+brew cask install disk-inventory-x 2> /dev/null
+brew cask install launchrocket 2> /dev/null
+brew cask install launchcontrol 2> /dev/null
+# brew cask install virtualbox 2> /dev/null
+# brew cask install colloquy 2> /dev/null
+brew cask install vlc 2> /dev/null
+brew cask install adobe-reader 2> /dev/null
+
+###############################################################################
+# Libs
+###############################################################################
+brew cask install liteicon 2> /dev/null
+# brew cask install imagealpha 2> /dev/null
+# brew cask install ksdiff 2> /dev/null
+# brew cask install flash 2> /dev/null
 # brew cask install java 2> /dev/null
 # brew cask install silverlight 2> /dev/null
 # brew cask install xquartz 2> /dev/null
