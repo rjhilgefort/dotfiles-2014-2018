aada2fc5307302ac3031ccf3505d5fad6dd8099a
Rob Hilgefort <rjhilgefort@gmail.com>
2016-11-17 23:46:00 -0500

    Clear the way for tmux as seperate repo

--- install.js
+++ install.js
@@ -149,7 +149,5 @@ if (shell.test('-d', photoshopDir)) {
 // Other commands that need to be run
 ///////////////////////////////////////////////////////////////////////////////
 
-// TODO: detect os and symlink either the mac or linux tmux file to `.tmux.conf.local`
-
 // Make sure we're using zsh
 shell.exec('chsh -s /bin/zsh');
