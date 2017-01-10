898a338e8dcf701a7dc1ade56fb0c389956944a9
Rob Hilgefort <rjhilgefort@gmail.com>
2017-01-09 20:22:15 -0500

    Cleanup, changes...
    - Got rid of "global" source and dest now that we're...
    - ...wrapping all the sections in their own methods, we have scope!
    - Short circuit rather than nested ifs
    - Updated template strings
    - Fixed photoshop symlink
    - Removed hard delete when link is found, respect option
    - Change default option to skip
    - Made all section headers

--- install.js
+++ install.js
@@ -16,7 +16,7 @@ const RE = require('./_lib/ramda-extras');
 program
   .version('0.0.2')
   .usage("[options]")
-  .option("-e, --existing [move]", "Handle existing directories as overwrite/[move]/skip", 'move')
+  .option("-e, --existing [skip]", "Handle existing directories as overwrite/move/[skip]", 'skip')
   .option("-u, --update", 'Update submodules before installing')
   .option("-s, --silent", 'Turn off logging')
   .parse(process.argv);
@@ -66,14 +66,13 @@ if (!program.silent) {
     return fn(...args);
   };
 
+  // TODO: Don't use `wrap`:
+  // http://stackoverflow.com/questions/41204219/ramda-how-to-modify-function-parameters-without-r-wrap/41210224
   S.exec = R.wrap(S.exec, makeVerbose(''));
   S.ln = R.wrap(S.ln, makeVerbose('ln'));
   S.rm = R.wrap(S.rm, makeVerbose('rm'));
   S.mv = R.wrap(S.mv, makeVerbose('mv'));
 }
-
-// Placeholder variable for all sections
-let source, dest;
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -83,7 +82,7 @@ echoSection('Make sure submodules are installed');
 (() => {
   if (!program.update) return S.echo('Skipping. Use `-u` flag to do so.');
   S.exec('git submodule update --init --recursive');
-})()
+})();
 
 ///////////////////////////////////////////////////////////////////////////////
 echoSection('Link base16-vim schemes to .vim dir');
@@ -98,121 +97,98 @@ echoSection('Link base16-vim schemes to .vim dir');
   _.forEach(S.ls(source), (scheme) => {
     S.ln('-sf', `${source}/${scheme}`, `${dest}/${scheme}`);
   });
-})()
+})();
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// echoSection('.prezto prompt installs');
+echoSection('.prezto prompt installs');
 ///////////////////////////////////////////////////////////////////////////////
+// TODO: Loop over all prompts in `source` and link. Hardcode the installs for now...
 
-// source = __dirname + '/.zprezto/modules/prompt/external';
-// dest = __dirname + '/.zprezto/modules/prompt/functions';
+(() => {
+  return S.echo('Skipping. TODO: Make sure this works...');
 
-/*
-// TODO: This would be ideal, but we'll just hardcode the installs for now
-S.ls(source).forEach(function(repo) {
-  repo = source + repo;
-});
-*/
+  let source = `${__dirname}/.zprezto/modules/prompt/external`;
+  let dest = `${__dirname}/.zprezto/modules/prompt/functions`;
 
-// pure
-// S.ln('-sf', `${source}/pure/pure.zsh`, `${dest}/prompt_pure_setup`);
+  // Pure
+  S.ln('-sf', `${source}/pure/pure.zsh`, `${dest}/prompt_pure_setup`);
+})();
 
 
 ///////////////////////////////////////////////////////////////////////////////
 echoSection('Link items at root of dotfiles project');
 ///////////////////////////////////////////////////////////////////////////////
 
-let projectFiles = [
-  '.git', '.gitignore', '.gitmodules', 'TODO.md', 'README.md',
-  'install.js', 'node_modules', 'package.json', 'npm-debug.log',
-  '.DS_Store', '_lib'
-];
-let special = ['.karabiner', '.spacemacs'];
-let blacklist = _.union(projectFiles, special);
+(() => {
+  let dest;
+  let projectFiles = [
+    '.git', '.gitignore', '.gitmodules', 'TODO.md', 'README.md',
+    'install.js', 'node_modules', 'package.json', 'npm-debug.log',
+    '.DS_Store', '_lib'
+  ];
+  let special = ['.spacemacs', '.adobe_photoshop'];
+  let blacklist = _.union(projectFiles, special);
 
-S.cd(__dirname);
+  _.forEach(S.ls('-A', __dirname), (nodeName) => {
+    // Skip iteration if in blacklist
+    if (_.includes(blacklist, nodeName)) return true;
 
-_.forEach(S.ls('-A'), (nodeName) => {
-  // Skip iteration if in blacklist
-  if (_.includes(blacklist, nodeName)) return true;
+    dest = `${S.env.HOME}/${nodeName}`;
 
-  dest = `${S.env.HOME}/${nodeName}`;
+    // Handle existing `dest` directories
+    if (S.test('-e', dest)) {
+      S.echo(`Existing target found, ${program.existing} applied to: ${dest}`);
 
-  // If it's a symlink, just remove it, regardless of the `existing` param
-  if (S.test('-L', dest)) {
-    S.rm('-f', dest);
-  }
-
-  // Handle existing `dest` directories
-  if (S.test('-e', dest)) {
-    switch (program.existing) {
-      case 'overwrite':
-        S.rm('-r', dest);
-        break;
-
-      case 'move':
-        S.mv('-f', dest, dest + "__");
-        break;
-
-      case 'skip':
-        break;
+      switch (program.existing) {
+        case 'overwrite':
+          S.rm('-r', dest);
+          break;
+        case 'move':
+          S.mv('-f', dest, `${dest}__`);
+          break;
+        case 'skip':
+          return true;
+          break;
+      }
     }
 
-    S.echo(`Existing target found, ${program.existing} applied to: ${dest}`);
-  }
+    let source = `${__dirname}/${nodeName}`;
+    S.ln('-sf', source, dest);
+  });
+})();
 
-  source = `${__dirname}/${nodeName}`;
-  S.ln('-sf', source, dest);
-});
-
-process.exit(1);
 
 ///////////////////////////////////////////////////////////////////////////////
-// Handle special configuration items
+echoSection('Handle Special: Spacemacs');
 ///////////////////////////////////////////////////////////////////////////////
-// TODO: Make function out of the special pattern
+// TODO: Make `ln` function that respects conflict directive (like in above loop)
 
-S.cd(__dirname);
+(() => {
+  S.ln('-sf', `${__dirname}/.spacemacs/.spacemacs`,               `${S.env.HOME}/.spacemacs`);
+  S.ln('-sf', `${__dirname}/.spacemacs/rjhilgefort`,              `${S.env.HOME}/.emacs.d/private/rjhilgefort`);
+  S.ln('-sf', `${__dirname}/.spacemacs/rjhilgefort/.mc-lists.el`, `${S.env.HOME}/.emacs.d/.mc-lists.el`);
+})();
 
-// Link spacemacs
-S.ln('-sf', `${__dirname}/.spacemacs/.spacemacs`,               `${S.env.HOME}/.spacemacs`);
-S.ln('-sf', `${__dirname}/.spacemacs/rjhilgefort`,              `${S.env.HOME}/.emacs.d/private/rjhilgefort`);
-S.ln('-sf', `${__dirname}/.spacemacs/rjhilgefort/.mc-lists.el`, `${S.env.HOME}/.emacs.d/.mc-lists.el`);
 
-// Link Karabiner config xml
-karabinerDir = `${S.env.HOME}/Library/Application\ Support/Karabiner`;
-karabinerFile = `${karabinerDir}/private.xml`;
-if (S.test('-d', karabinerDir)) {
-  // Move whatever is there, besides symlink
-  if (S.test('-e', karabinerFile) && !S.test('-L', karabinerFile)) {
-    S.mv(karabinerFile, karabinerFile + "_save");
-  }
-  // Link only if it's still not a symlink
-  if (!S.test('-L', karabinerFile)) {
-    S.ln('-sf', karabinerFile, `${__dirname}/.karabiner/private.xml`);
-  }
-}
-
-// Link adobe scripts if installed
+///////////////////////////////////////////////////////////////////////////////
+echoSection('Handle Special: Adobe Photoshop Scripts');
+///////////////////////////////////////////////////////////////////////////////
+// TODO: Make method out of the special pattern
 // TODO: loop over dotfiles photoshop directory and link all
-photoshopDir = "/Applications/Adobe Photoshop CS6/Presets/Scripts";
-photoshopFile = photoshopDir + "/BatchCropAndStriaghten.jsx";
-if (S.test('-d', photoshopDir)) {
+
+(() => {
+  const photoshopDir = "/Applications/Adobe Photoshop CS6/Presets/Scripts";
+  const photoshopFile =  + `${photoshopDir}/BatchCropAndStriaghten.jsx`;
+  if (!S.test('-d', photoshopDir)) return;
+
   // Move whatever is there, besides symlink
   if (S.test('-e', photoshopFile) && !S.test('-L', photoshopFile)) {
-    S.mv(photoshopFile, photoshopFile + "_save");
+    S.mv(photoshopFile, `${photoshopFile}_save`);
   }
+
   // Link only if it's still not a symlink
-  if (!S.test('-L', photoshopFile)) {
-    S.ln('-sf', photoshopFile, `${__dirname}/.adobe_photoshop/BatchCropAndStriaghten.jsx`);
-  }
-}
+  if (S.test('-L', photoshopFile)) return;
 
-///////////////////////////////////////////////////////////////////////////////
-// Other commands that need to be run
-///////////////////////////////////////////////////////////////////////////////
-
-// Make sure we're using zsh
-// TODO: Only do this if not zsh
-S.exec('chsh -s /bin/zsh');
+  S.ln('-sf', `${__dirname}/.adobe_photoshop/BatchCropAndStriaghten.jsx`, photoshopFile);
+})();
